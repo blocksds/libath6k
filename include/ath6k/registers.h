f10c8f3962dde6f6cb49f92fb6dac5c820629b7d
Antonio Niño Díaz <antonio_nd@proton.me>
2026-05-29 00:59:28 +0100

    registers: Add more definitions

--- include/ath6k/registers.h
+++ include/ath6k/registers.h
@@ -2,6 +2,9 @@
 //
 // Copyright (c) 2026 Antonio Niño Díaz
 
+// Note: This library is still a prototype. The names of the registers in this
+// file may change in the future.
+
 #ifndef LIBATH6K_REGISTERS_H__
 #define LIBATH6K_REGISTERS_H__
 
@@ -318,6 +321,150 @@ extern "C" {
 #define SIN_VAL             (*(vu32*)0x01C084)
 #define SW_SCLK             (*(vu32*)0x01C088)
 #define SW_CNTL             (*(vu32*)0x01C08C)
+
+// MAC PCU Registers
+// -----------------
+
+#define REG_STA_ID0                     (*(vu32*)0x028000) // MAC_PCU_STA_ADDR_L32
+#define REG_STA_ID1                     (*(vu32*)0x028004) // MAC_PCU_STA_ADDR_U16
+#define REG_BSS_ID0                     (*(vu32*)0x028008) // MAC_PCU_BSSID_L32
+#define REG_BSS_ID1                     (*(vu32*)0x02800C) // MAC_PCU_BSSID_U16
+#define MAC_PCU_REG_BCNRSSI             (*(vu32*)0x028010) // MAC_PCU_BCN_RSSI_AVE
+#define REG_TIME_OUT                    (*(vu32*)0x028014) // MAC_PCU_ACK_CTS_TIMEOUT
+#define MAC_PCU_REG_BCNSIG              (*(vu32*)0x028018) // MAC_PCU_BCN_RSSI_CTL
+#define REG_USEC                        (*(vu32*)0x02801C) // MAC_PCU_USEC_LATENCY
+#define REG_BEACON                      (*(vu32*)0x028020)
+#define REG_CFP_PERIOD                  (*(vu32*)0x028024) // (MAC_???)PCU_MAX_CFP_DUR
+#define REG_TIMER0                      (*(vu32*)0x028028)
+#define REG_TIMER1                      (*(vu32*)0x02802C)
+#define REG_TIMER2                      (*(vu32*)0x028030)
+#define REG_TIMER3                      (*(vu32*)0x028034)
+#define REG_CFP_DUR                     (*(vu32*)0x028038) // (MAC_???)PCU_MAX_CFP_DUR
+#define REG_RX_FILTER                   (*(vu32*)0x02803C) // MAC_PCU_RX_FILTER
+#define REG_MCAST_FIL0                  (*(vu32*)0x028040) // MAC_PCU_MCAST_FILTER_L32
+#define REG_MCAST_FIL1                  (*(vu32*)0x028044) // MAC_PCU_MCAST_FILTER_U32
+#define MAC_PCU_REG_DIAGSW              (*(vu32*)0x028048) // MAC_PCU_DIAG_SW
+#define REG_TSF_L32                     (*(vu32*)0x02804C)
+#define REG_TSF_U32                     (*(vu32*)0x028050)
+#define REG_TST_ADDAC                   (*(vu32*)0x028054) // MAC_PCU_TST_ADDAC
+#define REG_DEF_ANT                     (*(vu32*)0x028058) // MAC_PCU_DEF_ANTENNA
+#define MAC_PCU_REG_MUTE_MASKS0         (*(vu32*)0x02805C) // MAC_PCU_AES_MUTE_MASK_0
+#define MAC_PCU_REG_MUTE_MASKS1         (*(vu32*)0x028060) // MAC_PCU_AES_MUTE_MASK_1
+#define MAC_PCU_REG_GATED_CLKS          (*(vu32*)0x028064) // MAC_PCU_GATED_CLKS
+#define MAC_PCU_REG_OBS2                (*(vu32*)0x028068) // MAC_PCU_OBS_BUS_2
+#define MAC_PCU_REG_OBS1                (*(vu32*)0x02806C) // MAC_PCU_OBS_BUS_1
+
+#define REG_LAST_TSTP                   (*(vu32*)0x028080) // MAC_PCU_LAST_BEACON_TSF (?)
+#define REG_NAV                         (*(vu32*)0x028084) // MAC_PCU_NAV
+#define REG_RTS_OK                      (*(vu32*)0x028088) // MAC_PCU_RTS_SUCCESS_CNT
+#define REG_RTS_FAIL                    (*(vu32*)0x02808C) // MAC_PCU_RTS_FAIL_CNT
+#define REG_ACK_FAIL                    (*(vu32*)0x028090) // MAC_PCU_ACK_FAIL_CNT
+#define REG_FCS_FAIL                    (*(vu32*)0x028094) // MAC_PCU_FCS_FAIL_CNT
+#define REG_BEACON_CNT                  (*(vu32*)0x028098) // MAC_PCU_BEACON_CNT
+
+#define MAC_PCU_REG_XRMODE              (*(vu32*)0x0280C0) // MAC_PCU_XRMODE
+#define MAC_PCU_REG_XRDEL               (*(vu32*)0x0280C4) // MAC_PCU_XRDEL
+#define MAC_PCU_REG_XRTO                (*(vu32*)0x0280C8) // MAC_PCU_XRTO
+#define MAC_PCU_REG_XRCRP               (*(vu32*)0x0280CC) // MAC_PCU_XRCRP
+#define MAC_PCU_REG_XRSTMP              (*(vu32*)0x0280D0) // MAC_PCU_XRSTMP
+#define MAC_PCU_REG_SLP1                (*(vu32*)0x0280D4) // MAC_PCU_SLP1
+#define MAC_PCU_REG_SLP2                (*(vu32*)0x0280D8) // MAC_PCU_SLP2
+//#define MAC_PCU_REG_SLP3                (*(vu32*)0x0280DC) // MAC_PCU_SLP3
+#define MAC_PCU_REG_BSSMSKL             (*(vu32*)0x0280E0) // MAC_PCU_ADDR1_MASK_L32
+#define MAC_PCU_REG_BSSMSKH             (*(vu32*)0x0280E4) // MAC_PCU_ADDR1_MASK_U16
+#define MAC_PCU_REG_TPC                 (*(vu32*)0x0280E8) // MAC_PCU_TPC
+#define MAC_PCU_REG_TFC                 (*(vu32*)0x0280EC) // MAC_PCU_TX_FRAME_CNT
+#define MAC_PCU_REG_RFC                 (*(vu32*)0x0280F0) // MAC_PCU_RX_FRAME_CNT
+#define MAC_PCU_REG_RCC                 (*(vu32*)0x0280F4) // MAC_PCU_RX_CLEAR_CNT
+#define MAC_PCU_REG_CC                  (*(vu32*)0x0280F8) // MAC_PCU_CYCLE_CNT
+#define MAC_PCU_REG_QT1                 (*(vu32*)0x0280FC) // MAC_PCU_QUIET_TIME_1
+#define MAC_PCU_REG_QT2                 (*(vu32*)0x028100) // MAC_PCU_QUIET_TIME_2
+#define MAC_PCU_REG_TSF                 (*(vu32*)0x028104)
+#define MAC_PCU_REG_NOACK               (*(vu32*)0x028108) // MAC_PCU_QOS_NO_ACK
+#define MAC_PCU_REG_PHYERR              (*(vu32*)0x02810C) // MAC_PCU_PHY_ERROR_MASK
+#define MAC_PCU_REG_XRLAT               (*(vu32*)0x028110) // MAC_PCU_XRLAT
+#define MAC_PCU_REG_ACKSIFS_RESERVED    (*(vu32*)0x028114)
+#define MAC_PCU_REG_MICQOSCTL           (*(vu32*)0x028118) // MAC_PCU_MIC_QOS_CONTROL
+#define MAC_PCU_REG_MICQOSSEL           (*(vu32*)0x02811C) // MAC_PCU_MIC_QOS_SELECT
+#define MAC_PCU_REG_MISCMODE            (*(vu32*)0x028120) // MAC_PCU_MISC_MODE
+#define MAC_PCU_REG_FILTOFDM            (*(vu32*)0x028124) // MAC_PCU_FILTER_OFDM_CNT
+#define MAC_PCU_REG_FILTCCK             (*(vu32*)0x028128) // MAC_PCU_FILTER_CCK_CNT
+#define MAC_PCU_REG_PHYCNT1             (*(vu32*)0x02812C) // MAC_PCU_PHY_ERR_CNT_1
+#define MAC_PCU_REG_PHYCNTMASK1         (*(vu32*)0x028130) // MAC_PCU_PHY_ERR_CNT_1_MASK
+#define MAC_PCU_REG_PHYCNT2             (*(vu32*)0x028134) // MAC_PCU_PHY_ERR_CNT_2
+#define MAC_PCU_REG_PHYCNTMASK2         (*(vu32*)0x028138) // MAC_PCU_PHY_ERR_CNT_2_MASK
+#define MAC_PCU_REG_TSFTHRESH           (*(vu32*)0x02813C) // MAC_PCU_TSF_THRESHOLD
+//#define MAC_PCU_REG_TSFCAL              (*(vu32*)0x028140) // Misc
+#define MAC_PCU_REG_PHYERR_EIFS         (*(vu32*)0x028144) // MAC_PCU_PHY_ERROR_EIFS_MASK
+//#define MAC_PCU_REG_SYNC1               (*(vu32*)0x028148) // Time
+//#define MAC_PCU_REG_SYNC2               (*(vu32*)0x02814C) // Misc
+//#define MAC_PCU_REG_SYNC3               (*(vu32*)0x028150) // MCAST Addr_L
+//#define MAC_PCU_REG_SYNC4               (*(vu32*)0x028154) // MCAST Addr_U
+//#define MAC_PCU_REG_SYNC5               (*(vu32*)0x028158) // RX Time
+//#define MAC_PCU_REG_SYNC6               (*(vu32*)0x02815C) // INC
+//#define MAC_PCU_REG_SYNC7               (*(vu32*)0x028160) // Last Time
+//#define MAC_PCU_REG_SYNC8               (*(vu32*)0x028164) // Updated Time
+#define MAC_PCU_REG_PHYCNT3             (*(vu32*)0x028168) // MAC_PCU_PHY_ERR_CNT_3
+#define MAC_PCU_REG_PHYCNTMASK3         (*(vu32*)0x02816C) // MAC_PCU_PHY_ERR_CNT_3_MASK
+#define MAC_PCU_REG_BTMODE              (*(vu32*)0x028170) // MAC_PCU_BLUETOOTH_MODE
+#define MAC_PCU_REG_BTWEIGHT            (*(vu32*)0x028174) // MAC_PCU_BLUETOOTH_WEIGHTS
+#define MAC_PCU_REG_HCF                 (*(vu32*)0x028178) // MAC_PCU_HCF_TIMEOUT
+#define MAC_PCU_REG_BTMODE2             (*(vu32*)0x02817C) // MAC_PCU_BLUETOOTH_MODE2
+#define MAC_PCU_REG_BFCOEF1             ((vu32*)0x028180) // 0..7
+
+#define MAC_PCU_REG_BFCOEF2             (*(vu32*)0x0281C0)
+#define MAC_PCU_REG_KCMASK              (*(vu32*)0x0281C4)
+
+#define MAC_PCU_REG_TXSIFS              (*(vu32*)0x0281D0) // MAC_PCU_TXSIFS
+
+#define MAC_PCU_REG_TXOP_X              (*(vu32*)0x0281EC) // MAC_PCU_TXOP_X
+#define MAC_PCU_REG_TXOP_0_3            (*(vu32*)0x0281F0) // MAC_PCU_TXOP_0_3
+#define MAC_PCU_REG_TXOP_4_7            (*(vu32*)0x0281F4) // MAC_PCU_TXOP_4_7
+#define MAC_PCU_REG_TXOP_8_11           (*(vu32*)0x0281F8) // MAC_PCU_TXOP_8_11
+#define MAC_PCU_REG_TXOP_12_15          (*(vu32*)0x0281FC) // MAC_PCU_TXOP_12_15
+#define MAC_PCU_REG_GNRCTMR_N           ((vu32*)0x028200) // 0..7 | GENERIC_TIMERSxxx?
+#define MAC_PCU_REG_GNRCTMR_P           ((vu32*)0x028220) // 0..7 | GENERIC_TIMERSxxx?
+#define MAC_PCU_REG_GNRCTMR_M           (*(vu32*)0x028240) // MAC_PCU_GENERIC_TIMERS_MODE
+#define MAC_PCU_REG_SLP32_MODE          (*(vu32*)0x028244)
+#define MAC_PCU_REG_SLP32_WAKE          (*(vu32*)0x028248)
+#define MAC_PCU_REG_SLP32_TSF_INC       (*(vu32*)0x02824C)
+#define MAC_PCU_REG_SLPMIB1             (*(vu32*)0x028250)
+#define MAC_PCU_REG_SLPMIB2             (*(vu32*)0x028254)
+#define MAC_PCU_REG_SLPMIB3             (*(vu32*)0x028258)
+#define MAC_PCU_REG_MISCMODE2           (*(vu32*)0x02825C) // MAC_PCU_MISC_MODE2
+#define MAC_PCU_REG_SLP4                (*(vu32*)0x028260)
+#define MAC_PCU_REG_SLP5                (*(vu32*)0x028264)
+
+#define MAC_PCU_REG_MCICTL              (*(vu32*)0x028268)
+#define MAC_PCU_REG_MCIISR              (*(vu32*)0x02826C)
+#define MAC_PCU_REG_MCIIER              (*(vu32*)0x028270)
+#define MAC_PCU_REG_MCIWLP              (*(vu32*)0x028274)
+#define MAC_PCU_REG_MCIARW              (*(vu32*)0x028278)
+#define MAC_PCU_REG_MCIARR              (*(vu32*)0x02827C)
+#define MAC_PCU_REG_MCIADW              (*(vu32*)0x028280)
+#define MAC_PCU_REG_MCIADR              (*(vu32*)0x028284)
+#define MAC_PCU_REG_MCIFRW              (*(vu32*)0x028288)
+#define MAC_PCU_REG_MCIFRR              (*(vu32*)0x02828C)
+#define MAC_PCU_REG_MCIQRW              (*(vu32*)0x028290)
+#define MAC_PCU_REG_MCIQRR              (*(vu32*)0x028294)
+#define MAC_PCU_REG_MCIGRW              (*(vu32*)0x028298)
+#define MAC_PCU_REG_MCIGRR              (*(vu32*)0x02829C)
+#define MAC_PCU_REG_MCISTAT             (*(vu32*)0x0282A0)
+
+#define MAC_PCU_REG_BASIC_RATE_SET0     (*(vu32*)0x0282A4) // MAC_PCU_BASIC_RATE_SET0
+#define MAC_PCU_REG_BASIC_RATE_SET1     (*(vu32*)0x0282A8) // MAC_PCU_BASIC_RATE_SET1
+#define MAC_PCU_REG_BASIC_RATE_SET2     (*(vu32*)0x0282AC) // MAC_PCU_BASIC_RATE_SET2
+#define MAC_PCU_REG_SEC_BSSID_L32       (*(vu32*)0x0282B0) // MAC_PCU_BSSID2_L32
+#define MAC_PCU_REG_SEC_BSSID_U16       (*(vu32*)0x0282B4) // MAC_PCU_BSSID2_U16
+
+#define MAC_PCU_REG_FTYPE               ((vu32*)0x028500) // 0..0x3F
+
+#define MAC_PCU_REG_ACKSIFSMEM_RESERVED ((vu32*)0x028680) // 0..0x1F
+#define MAC_PCU_REG_DUR                 ((vu32*)0x028700) // 0..0x1F
+
+#define MAC_PCU_REG_RTD                 ((vu32*)0x0287C0) // 0..7
+#define MAC_PCU_REG_DTR                 ((vu32*)0x0287E0) // 0..7
+#define MAC_PCU_REG_KC                  ((vu32*)0x028800) // 0..0x3FF |  KC = KEY_CACHE ?
 
 #ifdef __cplusplus
 }
